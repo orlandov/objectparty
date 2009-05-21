884dd85f50661546251139d76a8a499991c0ce35
Orlando Vazquez <orlando@gossamer-threads.com>
2009-05-21 10:16:13 -0700

    serialize Reference'd objects, implicitly

--- tests/test_objectparty.py
+++ tests/test_objectparty.py
@@ -8,41 +8,51 @@ import uuid
 class Reference(object):
     def __init__(self, referent):
         self.referent = weakref.proxy(referent)
-        self.refid = None
+
 
 class Person(object):
     def __init__(self, **kwargs):
         self.__dict__ = kwargs
         self.__dict__['__class__'] = self.__class__.__name__
 
+
 class ObjectParty(object):
     def __init__(self):
         self.storage = {}
         self._seen_ids=[]
 
+    def mk_id(self):
+        return str(uuid.uuid4())
+
     def store(self, obj):
         if not obj.__dict__.get('id'):
-            obj.id = str(uuid.uuid4())
+            obj.id = self.mk_id()
 
+        # _seen_ids is a brutal hax, plz 2 fix
         self._seen_ids.append(obj.id)
 
         def reference_encoder(o):
-            _data = o.__dict__
+            # we're serializing a reference
+            if isinstance(o, Reference):
+                refid = o.referent.__dict__.get('id')
 
+                if not refid or (refid not in self._seen_ids and refid not in self.storage):
+                    refid = o.referent.id = self.mk_id()
+                    self.store(o.referent)
+
+                return { "$ref": refid }
+
+            # we're serializing an object
+            _data = o.__dict__
             for k in _data:
                 v = _data[k]
 
                 if isinstance(v, Reference):
-                    try:
-                        refid = v.referent.id
-                    except:
-                        refid = None
+                    refid = v.referent.__dict__.get('id')
 
-                    if refid and (refid in self._seen_ids or refid in self.storage):
-                        print "found c"
-                    else:
-                        refid = self.store(v.referent)
-                        v.referent.id = refid
+                    if not refid or (refid not in self._seen_ids and refid not in self.storage):
+                        refid = v.referent.id = self.mk_id()
+                        self.store(v.referent)
 
                     _data[k] = { "$ref": refid }
 
@@ -61,18 +71,23 @@ class TestObjectParty(unittest.TestCase):
         homer.spouse = Reference(marge)
         marge.spouse = Reference(homer)
 
+        p.store(homer)
+
+        homerobj = simplejson.loads(p.storage[homer.id])
+        margeobj = simplejson.loads(p.storage[marge.id])
+        self.assertEqual(homerobj['spouse']['$ref'], margeobj['id'])
+        self.assertEqual(margeobj['spouse']['$ref'], homerobj['id'])
 
         bart = Person(name='Bart')
         lisa = Person(name='Lisa')
+        homer.children = [Reference(bart), Reference(lisa)]
+        marge.children = [Reference(bart), Reference(lisa)]
         bart.father = Reference(homer)
         bart.mother = Reference(marge)
         lisa.father = Reference(homer)
         lisa.mother = Reference(marge)
 
-        homer.children = [Reference(bart), Reference(lisa)]
-        marge.children = [Reference(bart), Reference(lisa)]
 
-        p.store(homer)
         p.store(bart)
 
         import pprint
