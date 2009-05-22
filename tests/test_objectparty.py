c4df862ef0071473f163d66a69ee3a1b7620a3b0
Orlando Vazquez <orlando@gossamer-threads.com>
2009-05-22 12:14:22 -0700

    cache store'd object id() values and map them to uuids. - refactor Reference class to use weakref.ref instead of weakref.proxy so we can get   hard references when we need them (necessary to be able to id() the Reference referent

--- tests/test_objectparty.py
+++ tests/test_objectparty.py
@@ -7,44 +7,50 @@ import uuid
 
 class Reference(object):
     def __init__(self, referent):
-        self.referent = weakref.proxy(referent)
+        self._referent = weakref.ref(referent)
 
-class Base(object):
-    def __init__(self, **kwargs):
-        self.__dict__ = kwargs
-        self.__dict__['__class__'] = self.__class__.__name__
-
-class Person(Base):
-    pass
+    @property
+    def referent(self):
+        return self._referent()
 
 class ObjectParty(object):
     def __init__(self):
         self._storage = {}
         self._id_uuid = {}
-        self._seen_ids = []
+        self._seen_uuids = [] # TODO: turn this into a Set
 
     def mk_id(self):
         return str(uuid.uuid4())
+
+    def object_id(self, obj, **kwargs):
+        # check if we have an id for this object, if not, create one and map
+        # the object's id() (python buildin) value to the generated uuid
+        obj_id = id(obj)
+        uuid = self._id_uuid.get(obj_id)
+        if not uuid and kwargs.get('create'):
+            uuid = self.mk_id()
+            self._id_uuid[obj_id] = uuid
+
+        return uuid
+
 
     def _encode_object(self, obj):
         # instead of modifying the original object, we should create a map of
         # the real objects' id() (the python builtin) to storage uuids
 
-        if not obj.__dict__.get('id'):
-            obj.id = self.mk_id()
-
-        self._seen_ids.append(obj.id)
+        obj_uuid = self.object_id(obj, create=True)
+        self._seen_uuids.append(obj_uuid)
 
         def object_encoder(o):
             # we're serializing a reference
             if isinstance(o, Reference):
-                refid = o.referent.__dict__.get('id')
+                ref_uuid = self.object_id(o.referent)
 
-                if not refid or (refid not in self._seen_ids and refid not in self._storage):
-                    refid = o.referent.id = self.mk_id()
-                    self._encode_store(o.referent)
+                if not ref_uuid or (ref_uuid not in self._seen_uuids and ref_uuid not in self._storage):
+                    ref_uuid = self.object_id(o.referent, create=True)
+                    self._encode_object(o.referent)
 
-                return { "$ref": refid }
+                return { "$ref": ref_uuid }
 
             # we're serializing an object
             _data = o.__dict__.copy()
@@ -52,18 +58,19 @@ class ObjectParty(object):
                 v = _data[k]
 
                 if isinstance(v, Reference):
-                    refid = v.referent.__dict__.get('id')
+                    ref_uuid = self.object_id(v.referent)
 
-                    if not refid or (refid not in self._seen_ids and refid not in self._storage):
-                        refid = v.referent.id = self.mk_id()
+                    if not ref_uuid or (ref_uuid not in self._seen_uuids and ref_uuid not in self._storage):
+                        ref_uuid = self.object_id(v.referent, create=True)
                         self._encode_object(v.referent)
 
-                    _data[k] = { "$ref": refid }
+                    _data[k] = { "$ref": ref_uuid }
 
+            _data['id'] = self.object_id(o)
             return _data
 
-        self._storage[obj.id] = simplejson.dumps(obj, default=object_encoder)
-        return obj.id
+        self._storage[obj_uuid] = simplejson.dumps(obj, default=object_encoder)
+        return obj_uuid
 
     def store(self, obj):
         self._seen_ids = []
@@ -73,20 +80,51 @@ class ObjectParty(object):
     def get_undecoded(self, id):
         return self._storage[id]
 
+
+class Base(object):
+    def __init__(self, **kwargs):
+        self.__dict__ = kwargs
+        self.__dict__['__class__'] = self.__class__.__name__
+
+
+class Person(Base): pass
+
 class TestObjectParty(unittest.TestCase):
     def test_simple(self):
         p = ObjectParty()
 
         homer = Person(name='Homer')
+        homer_uuid = p.store(homer)
+        homerobj = simplejson.loads(p.get_undecoded(homer_uuid))
+
+        self.assert_(homerobj.has_key('id'))
+        self.assert_(homerobj['name'], 'Homer')
+
+    def test_one_reference(self):
+        p = ObjectParty()
+
+        homer = Person(name='Homer')
+        bart = Person(name='Bart')
+
+        homer.son = Reference(bart)
+
+        homer_uuid = p.store(homer)
+        bart_uuid = p.store(bart)
+
+        homerobj = simplejson.loads(p.get_undecoded(homer_uuid))
+        bartobj = simplejson.loads(p.get_undecoded(bart_uuid))
+
+        self.assertEqual(homerobj['son']['$ref'], bartobj['id'])
+
+        return
+
         marge = Person(name='Marge')
         homer.spouse = Reference(marge)
         marge.spouse = Reference(homer)
 
         homerdict=homer.__dict__.copy();
 
-        p.store(homer)
 
-        homerobj = simplejson.loads(p.get_undecoded(homer.id))
         margeobj = simplejson.loads(p.get_undecoded(marge.id))
 
         # check that the references are pointing to each other properly
@@ -100,13 +138,18 @@ class TestObjectParty(unittest.TestCase):
         lisa = Person(name='Lisa')
         homer.children = [Reference(bart), Reference(lisa)]
         marge.children = [Reference(bart), Reference(lisa)]
+
+        
         bart.father = Reference(homer)
         bart.mother = Reference(marge)
         lisa.father = Reference(homer)
         lisa.mother = Reference(marge)
 
 
+        p.store(homer)
+        p.store(marge)
         p.store(bart)
+        p.store(lisa)
 
         import pprint
         for k in p._storage:
